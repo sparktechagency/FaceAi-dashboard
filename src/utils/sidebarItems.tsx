b60189732690614b2ccdd546e135d1300607604c
MithilaKhan <mithilakhan082@gmail.com>
2025-11-29 15:53:00 +0600

    update layout

--- src/utils/sidebarItems.tsx
+++ src/utils/sidebarItems.tsx
@@ -16,85 +16,11 @@ const sidebarItems: TSidebarItem[] = [
         icon: '/icons/users.png',
     },
     {
-        key: 'package',
-        label: 'Package',
-        path: 'package',
-        icon: '/icons/package.png',
-    },
-    {
-        key: 'study-notes',
-        label: 'Study Notes',
-        path: 'study-notes',
-        icon: '/icons/study-notes.png',
-        children: [
-            {
-                key: 'np-study-notes',
-                label: 'NP Study Notes',
-                path: 'study-notes/np-study-notes',
-                icon: '/icons/study-notes.png',
-            },
-            {
-                key: 'rp-study-notes',
-                label: 'RP Study Notes',
-                path: 'study-notes/rp-study-notes',
-                icon: '/icons/study-notes.png',
-            },
-            {
-                key: 'lpn-study-notes',
-                label: 'LPN Study Notes',
-                path: 'study-notes/lpn-study-notes',
-                icon: '/icons/study-notes.png',
-            },
-        ],
-    },
-    {
-        key: 'body-system',
-        label: 'Body System',
-        path: 'body-system',
-        icon: '/icons/body-system.png',
-    },
-    {
-        key: 'patient-assessment',
-        label: 'Patient Assessment',
-        path: 'patient-assessment',
-        icon: '/icons/assessment.png',
-    },
-    {
-        key: 'clinical-skills',
-        label: 'Clinical Skills',
-        path: 'clinical-skills',
-        icon: '/icons/clinical-skill.png',
-    },
-    {
-        key: 'templates',
-        label: 'Templates',
-        path: 'templates',
-        icon: '/icons/templates.png',
-    },
-    {
-        key: 'exams',
-        label: 'Exams',
-        path: 'exam',
-        icon: '/icons/quiz.png',
-    },
-    {
-        key: 'flashcards',
-        label: 'Flashcards',
-        path: 'flashcards',
-        icon: '/icons/flashcard.png',
-    },
-    {
         key: 'settings',
         label: 'Settings',
         path: 'settings',
         icon: '/icons/setting.png',
         children: [
-            // {
-            //     key: 'about-us',
-            //     label: 'About us',
-            //     path: 'about-us',
-            //     icon: <TbBook size={20} />,
-            // },
             {
                 key: 'terms-and-condition',
                 label: 'Terms and Condition',
